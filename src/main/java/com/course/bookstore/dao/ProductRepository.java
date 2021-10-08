450e82518a8ad65e29f40efc61b1217355bb8b96
Olga Ivzhichenko <olga.ivzhichenko@kaltura.com>
2021-10-08 18:11:59 +0300

    first commit

---
+++ src/main/java/com/course/bookstore/dao/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.course.bookstore.dao;
+
+import com.course.bookstore.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
