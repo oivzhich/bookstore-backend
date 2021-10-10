192ccefc11ae4f248870283974900825be6bfdcd
Olga Ivzhichenko <olga.ivzhichenko@kaltura.com>
2021-10-10 11:46:29 +0300

    product search by category id added

--- src/main/java/com/course/bookstore/dao/ProductRepository.java
+++ src/main/java/com/course/bookstore/dao/ProductRepository.java
@@ -1,10 +1,13 @@
 package com.course.bookstore.dao;
 
 import com.course.bookstore.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
