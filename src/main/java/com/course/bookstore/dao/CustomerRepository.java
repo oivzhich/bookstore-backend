62bea6ac952cce1968556bc499511b289b926f03
Olga Ivzhichenko <olga.ivzhichenko@kaltura.com>
2021-10-17 11:52:55 +0300

    Don't add duplicate customers

--- src/main/java/com/course/bookstore/dao/CustomerRepository.java
+++ src/main/java/com/course/bookstore/dao/CustomerRepository.java
@@ -4,4 +4,7 @@ import com.course.bookstore.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Customer findByEmail(String theEmail);
+
 }
